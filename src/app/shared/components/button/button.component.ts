701248db574fd1a45ebdcb7b8928fdc26f52a49c
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-16 03:42:19 -0300

    feat: add button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+
+import { ButtonProps } from './button.interface'
+import { buttonStyles } from './button.component.styles'
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html'
+})
+export class ButtonComponent {
+  @Input()
+  buttonProps?: ButtonProps
+
+  @Output()
+  handleOnClick = new EventEmitter<Event>()
+
+  get className() {
+    return buttonStyles({
+      className: this.buttonProps?.class,
+      size: this.buttonProps?.size,
+      variant: this.buttonProps?.variant
+    })
+  }
+
+  onClick() {
+    this.handleOnClick.emit()
+  }
+}
---
+++ src/app/shared/components/button/button.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/angular'
+import { fn } from '@storybook/test'
+import { ButtonComponent } from './button.component'
+
+const meta: Meta<ButtonComponent> = {
+  title: 'Components/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  render: (args) => ({
+    props: args,
+    template: `<app-button>My Button</app-button>`
+  }),
+  args: { handleOnClick: fn() }
+}
+
+export default meta
+type Story = StoryObj<ButtonComponent>
+
+export const Primary: Story = {
+  args: {
+    buttonProps: {
+      type: 'button',
+      variant: 'default',
+      size: 'default'
+    }
+  }
+}
