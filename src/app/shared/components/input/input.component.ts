075096867b5967111a58a93e56a1211e1c057551
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-16 20:52:19 -0300

    feat: add input component

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core'
+
+import { InputProps } from './input.interface'
+
+@Component({
+	selector: 'app-input',
+	standalone: true,
+	imports: [],
+	templateUrl: './input.component.html'
+})
+export class InputComponent {
+	@Input() inputProps!: InputProps
+}
---
+++ src/app/shared/components/input/input.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/angular'
+import { InputComponent } from './input.component'
+
+const meta: Meta<InputComponent> = {
+	title: 'Components/Input',
+	component: InputComponent,
+	tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<InputComponent>
+
+export const Primary: Story = {}
